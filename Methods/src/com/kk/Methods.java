b9513b263a4fb408f21bddfa636b6af8571cf63b
sidhiq <mdsidhiq09@gmail.com>
2025-09-25 23:35:00 +0530

    add Methods

---
+++ Methods/src/com/kk/Methods.java
@@ -0,0 +1,15 @@
+package com.kk;
+
+public class Methods {
+
+	public static void main(String[] args) {
+System.out.println("This is inside main method");
+//calling the greetUser method
+greetUser();
+	}
+	public static void greetUser() {
+		System.out.println("hello user, you are inside \"greetUser\" method");
+	}
+	
+
+}
---
+++ Methods/src/com/kk/Methods2.java
@@ -0,0 +1,37 @@
+package com.kk;
+
+public class Methods2 {
+
+	public static void main(String[] args) {
+firstPattern();
+secondPattern();
+thirdPattern();
+	}
+	
+	public static void firstPattern() {
+		System.out.println("*");
+		System.out.println("* *");
+		System.out.println("* * *");
+		System.out.println("* * * *");
+		System.out.println("* * * * *");
+		System.out.println("\n\n\n");
+	}
+	
+	public static void secondPattern() {
+		System.out.println("* * * * *");
+		System.out.println("* * * * ");
+		System.out.println("* * * ");
+		System.out.println("* * ");
+		System.out.println("* ");
+		System.out.println("\n\n\n");
+	}
+	
+	public static void thirdPattern() {
+		System.out.println("        *");
+		System.out.println("      * *");
+		System.out.println("    * * *");
+		System.out.println("  * * * *");
+		System.out.println("* * * * *");
+	}
+
+}
