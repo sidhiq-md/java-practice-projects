aee1b6bbd515c7ebb56cb60a22f907b5383e6a52
sidhiq <mdsidhiq09@gmail.com>
2025-09-25 22:01:04 +0530

    Add Loops

---
+++ Loops/src/com/kk/WhileLoop.java
@@ -0,0 +1,20 @@
+package com.kk;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+int i = 0;
+while(i <= 10) {
+	System.out.println(i);
+	i+= 2;
+}
+
+System.out.println("\n \n \n");
+int j = 500;
+while(j>=200) {
+	System.out.println(j + "\n");
+	j-=10;
+}
+	}
+
+}
